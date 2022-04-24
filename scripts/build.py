9c0902b1f727af1cd677d0d42cf63cf6e191e372
AlterTobi <AlterTobi@users.noreply.github.com>
2022-04-24 19:23:06 +0200

    Update build.py
    warum erkennt codeQL das nicht?

--- scripts/build.py
+++ scripts/build.py
@@ -122,7 +122,7 @@ def run():
   # check if beta (issue or feature)
   ref = re.match('refs/heads/(\w+)(/[\w#]+)?',environ['GITHUB_REF'])
   if ref:
-    if (ref.group(1) == 'issue') || (ref.group(1) == 'feature'):
+    if (ref.group(1) == 'issue') | (ref.group(1) == 'feature'):
       name_or_number = ref.group(2)[1:] # no slash
       extra_version = '.beta'+environ['GITHUB_RUN_NUMBER']+'.'+ ref.group(1) + name_or_number
   

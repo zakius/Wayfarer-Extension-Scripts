5b15797c4bcef1660c866fbe1c3becb88d3e021c
AlterTobi <fotofreund0815@gmail.com>
2022-04-26 17:40:31 +0200

    . als Trenner

--- scripts/build.py
+++ scripts/build.py
@@ -125,7 +125,7 @@ def run():
     if ref.group(1) == 'issue':
       extra_version = '.beta'+environ['GITHUB_RUN_NUMBER']+'.issue' + ref.group(2)[2:]
     elif (ref.group(1) == 'feature')|(ref.group(1) == 'hotfix'):
-      extra_version = '.beta'+environ['GITHUB_RUN_NUMBER']+'.' + ref.group(1) + '-' + ref.group(2)[1:]
+      extra_version = '.beta'+environ['GITHUB_RUN_NUMBER']+'.' + ref.group(1) + '.' + ref.group(2)[1:]
   
   # process js files
   shortlist = []
